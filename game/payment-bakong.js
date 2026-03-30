95fbe0b9d8564339862c0cf97f66ad0d510bef92
Irra11 <boraffcremix@gmail.com>
2026-03-30 23:56:29 +0700

    Update PAYMENT_API URL for Bakong payment

--- game/payment-bakong.js
+++ game/payment-bakong.js
@@ -1,7 +1,7 @@
 /** 
  * SCRIPT 2: BAKONG PAYMENT & UI (Integrated with Digiflazz)
  */
-const PAYMENT_API = "https://concert-deutsch-females-himself.trycloudflare.com";
+const PAYMENT_API = "https://crm-falls-attacks-suggested.trycloudflare.com";
 let selectedPackage = null;
 let pollInterval = null;
 
